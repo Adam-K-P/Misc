f2951554cfcf5ea36bc6729a67c85fe64aa09557
adam <akpinarb@ucsc.edu>
2015-09-23 16:45:01 -0700

    added right and left rotations

--- Algorithm_practice/Red_black/red_black.java
+++ Algorithm_practice/Red_black/red_black.java
@@ -1,4 +1,5 @@
 //Adam Pinarbasi
+//red_black
 //Red-black tree implementation in Java
 
 class red_black {
@@ -13,7 +14,8 @@ class red_black {
       node right;
       node left;
    }
-   static node root = null;
+   node root = null;
+   node nil  = new node ();
 
    //methods
 
@@ -32,33 +34,33 @@ class red_black {
       return null;
    }
 
-   void bt_insert (int key) {
+   node bt_insert (int key) {
       if (root == null) {
          node x   = new node();
          x.key    = key;
          x.color  = 0;
          x.parent = null;
-         x.right  = null;
-         x.left   = null;
+         x.right  = nil;
+         x.left   = nil;
          root     = x;
-         return;
+         return root;
       }
 
       for (node curr = root; curr != null;) {
-         if (key == curr.key) return;
+         if (key == curr.key) return null;
 
          node x   = new node();
          x.key    = key;
          x.color  = 1;
-         x.parent = null
-         x.right  = null;
-         x.left   = null;
+         x.parent = null;
+         x.right  = nil;
+         x.left   = nil;
 
          if (key < curr.key) {
             if (curr.left == null) {
                x.parent  = curr;
                curr.left = x;
-               return;
+               return x;
             }
             else curr = curr.left;
          }
@@ -67,20 +69,92 @@ class red_black {
             if (curr.right == null) {
                x.parent   = curr;
                curr.right = x;
-               return;
+               return x;
             }
             else curr = curr.right;
          }
       }
+      return null;
+   }
+
+   //precondition: element.right != null
+   void left_rotate (node element) {
+      node y = element.right;
+      element.right = y.left;
+      if (y.left != nil) 
+         y.left.parent = element;
+      y.parent = element.parent;
+      if (element.parent == nil) 
+         root = y;
+      else if (element == element.parent.left)
+         element.parent.left = y;
+      else 
+         element.parent.right = y;
+      y.left = element;
+      element.parent = y;
+   }
+
+   void right_rotate (node element) {
+      node y = element.left;
+      element.left = y.right;
+      if (y.right != nil) 
+         y.right.parent = element;
+      y.parent = element.parent;
+      if (element.parent == nil)
+         root = y;
+      else if (element == element.parent.right)
+         element.parent.right = y;
+      else
+         element.parent.left = y;
+      y.right = element;
+      element.parent = y;
    }
 
    void insert (int key) {
-      bt_insert(key);
+      node inserted = bt_insert(key);
+      if (inserted == null) return;
+      inserted.color = 1;
+      while (inserted != root && inserted.color == 1) {
+         if (inserted.parent == inserted.parent.parent.left) {
+            node x = inserted.parent.parent.right;
+            if (x.color == 1) { 
+               //case 1
+            }
+            else {
+               if (x == inserted.parent.right) {
+                  //case 2
+               }
+               //case 3
+            }
+         }
+         else {
+            node x = inserted.parent.parent.left;
+            if (x.color == 1) {
+               //case 1
+            }
+            else {
+               if (x == inserted.parent.left) {
+                  //case 2
+               }
+               //case 3
+            }
+         }
+      }
    }
    
-   void delete (int key) {}
+   //fails if key corresponds to root
+   void delete (int key) {
+   }
 
-   static void print () {
+   void print () {
+      print_element(root);
+   }
+
+   void print_element (node element) {
+      if (element == null) return;
+      print_element(element.left);
+      System.out.printf("%d\n", element.key);
+      print_element(element.right);
    }
 }
 
--- Algorithm_practice/Red_black/red_black_test.java
+++ Algorithm_practice/Red_black/red_black_test.java
@@ -2,8 +2,15 @@ class red_black_test {
 
    public static void main (String[] args) {
       red_black test = new red_black();
+
       test.insert(1);
       test.insert(2);
+      test.insert(0);
+      test.insert(5);
+      test.insert(4);
+      test.insert(10);
+      test.insert(3);
+
       test.print();
    }
 }
