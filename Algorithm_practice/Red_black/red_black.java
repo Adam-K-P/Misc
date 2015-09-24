081dd430dec8e58a6309ac3af9a48ea780b5b381
adam <akpinarb@ucsc.edu>
2015-09-24 11:39:18 -0700

    worked on rbt insertion

--- Algorithm_practice/Red_black/red_black.java
+++ Algorithm_practice/Red_black/red_black.java
@@ -24,10 +24,8 @@ class red_black {
       for (node curr = root; curr != null;) {
          if (key == curr.key) 
             return curr.value;
-
          if (key < curr.key) 
             curr = curr.left;
-
          if (key > curr.key) 
             curr = curr.right;
       }
@@ -57,16 +55,15 @@ class red_black {
          x.left   = nil;
 
          if (key < curr.key) {
-            if (curr.left == null) {
+            if (curr.left == nil) {
                x.parent  = curr;
                curr.left = x;
                return x;
             }
             else curr = curr.left;
          }
-
          else {
-            if (curr.right == null) {
+            if (curr.right == nil) {
                x.parent   = curr;
                curr.right = x;
                return x;
@@ -77,72 +74,106 @@ class red_black {
       return null;
    }
 
-   //precondition: element.right != null
-   void left_rotate (node element) {
-      node y = element.right;
-      element.right = y.left;
-      if (y.left != nil) 
-         y.left.parent = element;
-      y.parent = element.parent;
-      if (element.parent == nil) 
-         root = y;
-      else if (element == element.parent.left)
-         element.parent.left = y;
-      else 
-         element.parent.right = y;
-      y.left = element;
-      element.parent = y;
+   //temporary test function
+   void do_left (int key) { 
+      for (node curr = root; curr != nil; ) {
+         if (curr.key == key) {
+            right_rotate(curr);
+            break;
+         }
+         else if (key < curr.key)
+            curr = curr.left;
+         else
+            curr = curr.right;
+      }
+      System.out.printf("%d\n%d\n", root.key, root.right.key);
    }
 
-   void right_rotate (node element) {
-      node y = element.left;
-      element.left = y.right;
-      if (y.right != nil) 
-         y.right.parent = element;
-      y.parent = element.parent;
-      if (element.parent == nil)
-         root = y;
-      else if (element == element.parent.right)
-         element.parent.right = y;
+   //precondition: element.right != nil
+   void left_rotate (node rooted) {
+      node pivot = rooted.right;
+      rooted.right = pivot.left;
+      if (pivot.left != nil) 
+         pivot.left.parent = rooted;
+      pivot.parent = rooted.parent;
+      if (rooted.parent == null) 
+         root = pivot;
+      else if (rooted == rooted.parent.left)
+         rooted.parent.left = pivot;
+      else 
+         rooted.parent.right = pivot;
+      pivot.left = rooted;
+      rooted.parent = pivot;
+   }
+
+   //precondition: element.left != nil
+   void right_rotate (node rooted) {
+      node pivot = rooted.left;
+      rooted.left = pivot.right;
+      if (pivot.right != nil) 
+         pivot.right.parent = rooted;
+      pivot.parent = rooted.parent;
+      if (rooted.parent == null)
+         root = pivot;
+      else if (rooted == rooted.parent.right)
+         rooted.parent.right = pivot;
       else
-         element.parent.left = y;
-      y.right = element;
-      element.parent = y;
+         rooted.parent.left = pivot;
+      pivot.right = rooted;
+      rooted.parent = pivot;
    }
 
    void insert (int key) {
       node inserted = bt_insert(key);
       if (inserted == null) return;
       inserted.color = 1;
-      while (inserted != root && inserted.color == 1) {
+      while (inserted != root && inserted.parent.color == 1) {
          if (inserted.parent == inserted.parent.parent.left) {
             node x = inserted.parent.parent.right;
             if (x.color == 1) { 
                //case 1
+               inserted.parent.color = 0;
+               x.color = 0;
+               inserted.parent.parent.color = 1;
+               inserted = inserted.parent.parent;
             }
             else {
-               if (x == inserted.parent.right) {
+               if (inserted == inserted.parent.right) {
                   //case 2
+                  inserted = inserted.parent;
+                  left_rotate(inserted);
                }
                //case 3
+               inserted.parent.color = 0;
+               inserted.parent.parent.color = 1;
+               right_rotate(inserted.parent.parent);
             }
          }
          else {
             node x = inserted.parent.parent.left;
             if (x.color == 1) {
-               //case 1
+               //case 4
+               inserted.parent.color = 0;
+               x.color = 0;
+               inserted.parent.parent.color = 1;
+               inserted = inserted.parent.parent;
             }
             else {
-               if (x == inserted.parent.left) {
-                  //case 2
+               if (inserted == inserted.parent.left) {
+                  //case 5
+                  inserted = inserted.parent;
+                  right_rotate(inserted);
                }
-               //case 3
+               //case 6
+               inserted.parent.color = 0;
+               inserted.parent.parent.color = 1;
+               left_rotate(inserted.parent.parent);
             }
          }
       }
+      root.color = 0;
    }
    
-   //fails if key corresponds to root
    void delete (int key) {
    }
 
@@ -151,7 +182,7 @@ class red_black {
    }
 
    void print_element (node element) {
-      if (element == null) return;
+      if (element == nil) return;
       print_element(element.left);
       System.out.printf("%d\n", element.key);
       print_element(element.right);
