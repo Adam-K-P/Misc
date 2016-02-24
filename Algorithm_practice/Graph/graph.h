c805b0510b8919d886666e08b50b4f46339f2221
Adam-K-P <akpinarb@ucsc.edu>
2016-02-23 20:12:56 -0800

    worked on bfs implementation a little more

--- Algorithm_practice/Graph/graph.h
+++ Algorithm_practice/Graph/graph.h
@@ -11,14 +11,14 @@ class graph {
 
    private:
       std::unordered_map<T, std::unordered_set<T>*>* neighbors;
-      std::unordered_map<T, T>* parent;
-      std::vector<T>* level;
+      std::unordered_map<T, std::unordered_set<T>*>* parent;
+      std::unordered_map<T, int>* level;
 
    public:
       graph () {
          neighbors = new std::unordered_map<T, std::unordered_set<T>*>;
          parent = new std::unordered_map<T, T>;
-         level = new std::vector<T>;
+         level = new std::unordered_map<T, T>;
       }
 
       ~graph () {
@@ -27,17 +27,31 @@ class graph {
          delete level;
       }
 
+      //could sort if I wanted
+      void print () {
+         for (auto it = neighbors->begin (); it != neighbors->end (); ++it) {
+            std::cout << "Vertex: " << it->first << std::endl;
+            for (auto jt = it->second->begin (); 
+                           jt != it->second->end (); 
+                           ++jt) 
+               std::cout << "     Neighbor: " << *jt << std::endl;
+         }
+      }
+
       void add_vertex (T vertex) {
          std::unordered_set<T>* neighbors_ = new std::unordered_set<T>;
-         neighbors->insert (vertex, neighbors_);
+         neighbors->emplace (vertex, neighbors_);
       }
 
       void add_arc (T vertex, T vertex_) {
          std::unordered_set<T>* neighbors_ = neighbors->at (vertex);
-         neighbors_->insert (vertex_);
+         neighbors_->emplace (vertex_);
       }
 
       void bfs (T source) {
+         level->emplace (source, 0);
+         int i = 1;
+         for (auto it = neighbors->begin (); it != neighbors->end (); ++it)
       }
 
 };
--- Algorithm_practice/Graph/main.cpp
+++ Algorithm_practice/Graph/main.cpp
@@ -7,6 +7,40 @@ using namespace std;
 
 int main () {
    graph<int>* this_graph = new graph<int>;
-   this_graph->bfs (2);
+
+   this_graph->add_vertex (1); 
+   this_graph->add_vertex (2);
+   this_graph->add_vertex (3);
+   this_graph->add_vertex (4); 
+   this_graph->add_vertex (5); 
+   this_graph->add_vertex (6); 
+   this_graph->add_vertex (7); 
+   this_graph->add_vertex (8); 
+   this_graph->add_vertex (9); 
+   this_graph->add_vertex (10); 
+   this_graph->add_vertex (11); 
+   this_graph->add_vertex (12); 
+   this_graph->add_vertex (13); 
+   this_graph->add_vertex (14); 
+   this_graph->add_vertex (15); 
+   this_graph->add_vertex (16); 
+   this_graph->add_vertex (17); 
+   this_graph->add_vertex (18);
+   this_graph->add_vertex (19);
+   this_graph->add_vertex (20);
+
+   this_graph->add_arc (1, 20);
+   this_graph->add_arc (2, 19);
+   this_graph->add_arc (3, 18);
+   this_graph->add_arc (4, 17);
+   this_graph->add_arc (5, 16);
+   this_graph->add_arc (6, 15);
+   this_graph->add_arc (7, 14);
+   this_graph->add_arc (8, 13);
+   this_graph->add_arc (9, 12);
+   this_graph->add_arc (10, 11);
+     
+   
+   this_graph->print ();
    return EXIT_SUCCESS;
 }
