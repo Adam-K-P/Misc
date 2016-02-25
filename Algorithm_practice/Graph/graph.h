d07f42015f2f3ec766ea64c9760b93c92c25a411
Adam-K-P <akpinarb@ucsc.edu>
2016-02-24 21:16:46 -0800

    worked on graph some more

--- Algorithm_practice/Graph/graph.h
+++ Algorithm_practice/Graph/graph.h
@@ -6,18 +6,21 @@
 #include <unordered_set>
 #include <vector>
 
+
 template<typename T>
 class graph {
 
+   using adj = std::unordered_set<T>;
+
    private:
-      std::unordered_map<T, std::unordered_set<T>*>* neighbors;
-      std::unordered_map<T, std::unordered_set<T>*>* parent;
+      std::unordered_map<T, adj*>* neighbors;
+      std::unordered_map<T, T>* parent;
       std::unordered_map<T, int>* level;
 
    public:
       graph () {
-         neighbors = new std::unordered_map<T, std::unordered_set<T>*>;
-         parent = new std::unordered_map<T, std::unordered_set<T>*>;
+         neighbors = new std::unordered_map<T, adj*>;
+         parent = new std::unordered_map<T,T>;
          level = new std::unordered_map<T, int>;
       }
 
@@ -27,10 +30,13 @@ class graph {
          delete level;
       }
 
-      //could sort if I wanted
+      //maybe sort?
       void print () {
          for (auto it = neighbors->begin (); it != neighbors->end (); ++it) {
             std::cout << "Vertex: " << it->first << std::endl;
+            if (parent->find (it->first) != parent->end ()) 
+               std::cout << "   Parent: " << parent->at (it->first) 
+                         << std::endl;
             for (auto jt = it->second->begin (); 
                            jt != it->second->end (); 
                            ++jt) 
@@ -39,20 +45,34 @@ class graph {
       }
 
       void add_vertex (T vertex) {
-         std::unordered_set<T>* neighbors_ = new std::unordered_set<T>;
+         adj* neighbors_ = new adj;
          neighbors->emplace (vertex, neighbors_);
       }
 
       void add_arc (T vertex, T vertex_) {
-         std::unordered_set<T>* neighbors_ = neighbors->at (vertex);
-         neighbors_->emplace (vertex_);
+         if (neighbors != nullptr and 
+            neighbors->find (vertex) != neighbors->end ()) {
+            adj* neighbors_ = neighbors->at (vertex);
+            neighbors_->insert (vertex_);
+         }
       }
 
       void bfs (T source) {
          level->emplace (source, 0);
          int i = 1;
+         for (auto it = neighbors->at(source)->begin ();
+                   it != neighbors->at(source)->end ();
+                   ++it, ++i) {
+            for (auto jt = neighbors->at(*it)->begin ();
+                      jt != neighbors->at(*it)->end ();
+                      ++jt) {
+               if (level->find (*jt) == level->end ()) {
+                  level->emplace (*jt, i);
+                  parent->emplace (*jt, *it);
+               }
+            }
+         }
       }
-
 };
 
 #endif
